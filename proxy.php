7d2c0e97a86b52f1340e69df6d88e744289dee74
Jens Reimann <jreimann@redhat.com>
2023-01-16 22:23:01 +0100

    chore: don't log API calls

--- proxy.php
+++ proxy.php
@@ -11,7 +11,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $log = new Logger('api');
-$log->pushHandler(new StreamHandler(__DIR__.'/var/logs/api.log'));
+//$log->pushHandler(new StreamHandler(__DIR__.'/var/logs/api.log'));
 
 $url = $_GET['url'] ?? null;
 $method = $_SERVER['REQUEST_METHOD'];
