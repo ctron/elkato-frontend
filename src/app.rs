3c201cf28e883df860877fb68a69c30e59f3158b
Jens Reimann <jreimann@redhat.com>
2023-01-29 17:30:05 +0100

    fix: use correct proxy url

--- src/app.rs
+++ src/app.rs
@@ -3,13 +3,14 @@ use elkato_api::Credentials;
 use pages::{index::Index, Pages};
 use patternfly_yew::*;
 use yew::prelude::*;
-use yew_hooks::{use_session_storage, use_update, UseSessionStorageHandle};
+use yew_hooks::{use_session_storage, UseSessionStorageHandle};
 use yew_nested_router::{prelude::*, Switch as RouterSwitch};
 
 pub const API_URL: &str = "https://www.elkato.de/buchung/";
 
 #[cfg(not(debug_assertions))]
-pub const CORS_API_URL: &str = "https://elkato.dentrassi.de/url=https://www.elkato.de/buchung/";
+pub const CORS_API_URL: &str =
+    "https://elkato.dentrassi.de/proxy.php?url=https://www.elkato.de/buchung/";
 #[cfg(debug_assertions)]
 pub const CORS_API_URL: &str = "http://localhost:9999/https://www.elkato.de/buchung/";
 
