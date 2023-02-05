5d9bd21e6d953e5ead59230f409c0c422712e0c6
Jens Reimann <jreimann@redhat.com>
2023-02-05 15:53:09 +0100

    refactor: use a different PHP CORS proxy

--- proxy.php
+++ proxy.php
@@ -1,83 +1,8 @@
 <?php
 
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Symfony\Component\HttpClient\HttpClient;
+require 'php/Geekality/CrossOriginProxy.php';
 
-require __DIR__.'/vendor/autoload.php';
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-$log = new Logger('api');
-//$log->pushHandler(new StreamHandler(__DIR__.'/var/logs/api.log'));
-
-$url = $_GET['url'] ?? null;
-
-if (!str_starts_with($url, "https://www.elkato.de/")) {
-  http_response_code(400);
-  exit(0);
-}
-
-$method = $_SERVER['REQUEST_METHOD'];
-$headers = getallheaders();
-
-$toUnset = [
-    'Referer',
-    'Cookie',
-    'Upgrade-Insecure-Requests',
-    'Accept-Encoding',
-    'Cache-Control',
-    'Mod-Rewrite',
-    'Host',
-];
-
-foreach ($toUnset as $key) {
-    unset($headers[$key]);
-}
-
-foreach ($headers as $key => $header) {
-    if (str_starts_with($key, 'Sec-')) {
-        unset($headers[$key]);
-    }
-}
-
-//$headers['Content-Type'] = 'application/json';
-
-$log->info('API request received', ['url' => $url, 'method' => $method, 'headers' => $headers]);
-if ('POST' === $method) {
-    $log->debug('POST', $_POST);
-}
-
-$client = HttpClient::create();
-
-switch ($method) {
-    case 'GET':
-        unset($headers['Content-Length']);
-        $response = $client->request(
-            $method,
-            $url,
-            [
-                'headers' => $headers,
-            ]
-        );
-        break;
-
-    case 'POST':
-        $response = $client->request(
-            $method,
-            $url,
-            [
-                'headers' => $headers,
-                'body' => file_get_contents('php://input'),
-            ]
-        );
-        break;
-}
-
-
-$log->debug('Server responded', ['response-code' => $response->getStatusCode(), 'headers' => $response->getHeaders(false)]);
-
-http_response_code($response->getStatusCode());
-echo $response->getContent(false);
+Geekality\CrossOriginProxy::proxy([
+    ['host' => 'elkato.de', 'scheme' => 'https'],
+    ['host' => 'www.elkato.de', 'scheme' => 'https'],
+]);
